de500e0cfae7a5cf29f22f0cd4a6ab1563135ce7
ameir <ameir@users.noreply.github.com>
2026-06-09 11:31:40 -0400

    build: add aarch64 target to build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ mkdir -p dist
 
 GOARCH=amd64 CGO_ENABLED=0 go build -ldflags="-s -w" -o "dist/${BIN}_linux_amd64"
 GOARCH=arm GOARM=7 CGO_ENABLED=0 go build -ldflags="-s -w" -o "dist/${BIN}_linux_armv7"
+GOARCH=arm64 CGO_ENABLED=0 go build -ldflags="-s -w" -o "dist/${BIN}_linux_arm64"
